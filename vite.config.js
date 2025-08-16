29be1ac16fbabec1abd7bbe14ca235b2e6b218f7
Mamadou98606 <78264446+Mamadou98606@users.noreply.github.com>
2025-08-17 01:02:19 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,19 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  server: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 3000,
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 3000,
+  }
 })
