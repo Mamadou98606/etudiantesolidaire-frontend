7dfd935c9e9e9c859c75b357959f69f9df157d76
Mamadou98606 <78264446+Mamadou98606@users.noreply.github.com>
2025-09-05 09:18:00 +0200

    Update progress.jsx

--- src/components/ui/progress.jsx
+++ src/components/ui/progress.jsx
@@ -1,6 +1,4 @@
 import * as React from "react"
-import * as ProgressPrimitive from "@radix-ui/react-progress"
-
 import { cn } from "@/lib/utils"
 
 function Progress({
@@ -9,18 +7,20 @@ function Progress({
   ...props
 }) {
   return (
-    <ProgressPrimitive.Root
+    <div
       data-slot="progress"
       className={cn(
         "bg-primary/20 relative h-2 w-full overflow-hidden rounded-full",
         className
       )}
-      {...props}>
-      <ProgressPrimitive.Indicator
+      {...props}
+    >
+      <div
         data-slot="progress-indicator"
         className="bg-primary h-full w-full flex-1 transition-all"
-        style={{ transform: `translateX(-${100 - (value || 0)}%)` }} />
-    </ProgressPrimitive.Root>
+        style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
+      />
+    </div>
   );
 }
 
